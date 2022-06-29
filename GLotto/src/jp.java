a52f1c4d67c747514597ff4d8952cafe3076d5ad
Administrator <Administrator@SC-202106161522>
2022-06-29 12:00:59 +0900

    SFDG

--- GLotto/src/jp.java
+++ GLotto/src/jp.java
@@ -1,17 +1,14 @@
 
 public class jp {
 	public static void main(String args[]) {
-<<<<<<< HEAD
 	System.out.println("�谡 ���Ŀ�");
 	
 	System.out.println("한글 안깨지는뎅");
-=======
 	System.out.println("�谡 ���Ŀ�");
 	System.out.println("�谡 ���");
 	System.out.println("abcd");
 	
 	
->>>>>>> branch 'master' of https://github.com/junhyeokkk/GLotto.git
 	System.out.println("asdfasdf");
 	System.out.println("한글은왜 ㅅㅂ");
 	}
