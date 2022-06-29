2c3b45e0a38f452311480c0f8e5f6051dd86d583
Administrator <Administrator@SC-202106161522>
2022-06-29 12:06:20 +0900

    한글깨진거 다 지움

--- GLotto/src/jp.java
+++ GLotto/src/jp.java
@@ -1,11 +1,8 @@
 
 public class jp {
 	public static void main(String args[]) {
-	System.out.println("�谡 ���Ŀ�");
 	
 	System.out.println("한글 안깨지는뎅");
-	System.out.println("�谡 ���Ŀ�");
-	System.out.println("�谡 ���");
 	System.out.println("abcd");
 	
 	
