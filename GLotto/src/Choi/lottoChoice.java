875b9ce400c5d7737a45bd97ee0823f5aeab383f
Administrator <Administrator@SC-202106161522>
2022-07-01 10:30:53 +0900

    리셋버튼 오류 고침 리셋버튼 정상작동 확인버튼 눌리면 누른 숫자 확인가능 (6개 다 찼을때만 가능)

--- GLotto/src/Choi/lottoChoice.java
+++ GLotto/src/Choi/lottoChoice.java
@@ -27,7 +27,11 @@ public class lottoChoice extends JFrame {
 	public lottoChoice() {
 		JPanel pnl = new JPanel();
 		JLabel titleLabel = new JLabel("번호입력");
-		JLabel ChoiceNumberAll = new JLabel("선택한 번호");
+		JLabel UserNumberImage[] = new JLabel[6];
+		for (int i = 0; i < UserNumberImage.length; i++) {
+			UserNumberImage[i] = new JLabel("번호 이미지" + (i));
+			pnl.add(UserNumberImage[i]);
+		}
 		NumberToImages numberToImage = new NumberToImages();
 		JButton autoButton = new JButton("자동버튼");
 		JButton resetButton = new JButton("RESET");
@@ -41,18 +45,21 @@ public class lottoChoice extends JFrame {
 			pnl.add(lblAll[i]);
 			////////////Label 이벤트 45개 만들었다 + 라벨 클릭하면 번호 담는 메소드 추가함/////////////////
 			lblAll[i].addMouseListener(new MouseAdapter() {
-				int clickcount = 0; // 해당 라벨을 클릭 하는 횟수
+				int clickcount = 0; // 해당 라벨을 클릭 하는 횟수 	// 밖에 넣은 이유는 안에 넣으면 계속 클릭횟수 1번으로만 인식한다.
 				@Override
 				public void mousePressed(MouseEvent e) {
 					JLabel clickLabel = (JLabel) e.getSource(); // 이벤트가 발생하는 라벨을 clickLabel라는 새로운 Label을 만들었다.
+					if (choiceLottoNumbers.size() == 0) {	// Reset했을때 clickcount는 리셋이 안되어서 리셋하는 조건문을 만들었다.
+						clickcount = 0;
+					}
 					if (choiceLottoNumbers.size() < 7) {	//	6개 다찼을때는 조건문이 바로 나가져서 Label 취소가 안된다. 그래서 size를 6에서 7로 바꾸었다. 번호 추가는 6개만 되게 했다.
 						for (int i = 0; i < lblAll.length; i++) {
-							// 클릭 이벤트가 발생한 객체의 source와 label이미지객체의 source가 같으면 click 횟수가 증가한다. 나중에 삭제할때 쓸려고
-							// 횟수 알게했다.
-							if (clickLabel.equals(lblAll[i])) {
-								clickcount++;
-								clickNumber = (i + 1);
-								System.out.println("" + clickNumber + "번호 클릭 횟수" + clickcount);
+							
+							if (clickLabel.equals(lblAll[i])) { // 클릭 이벤트가 발생한 객체의 source와 label이미지객체의 source가 같으면 click 횟수가 증가한다. 나중에 삭제할때 쓸려고
+								clickcount++;					// 횟수 알게했다.
+								clickNumber = (i + 1);			// 클릭라벨과 (lblAll 라벨의 숫자 + 1) 값 같으면 그 숫자가 나오게 했다. 
+								System.out.println("클릭한 이미지 번호 = " + clickNumber + ", 클릭 횟수 = " + clickcount);
+								
 							}
 						}
 						if (clickcount % 2 != 0 && choiceLottoNumbers.size() < 6) {
@@ -66,7 +73,9 @@ public class lottoChoice extends JFrame {
 						}
 						System.out.println("set" + choiceLottoNumbers.toString());
 						// set으로 가져온 값들을 오름차순 정렬했다.
-						UserGetLottoNumbers = new ArrayList<Integer>(choiceLottoNumbers);		// 참조값이 바뀌는거 같다.
+						
+						UserGetLottoNumbers = new ArrayList<Integer>(choiceLottoNumbers);		// 참조값이 바뀌는거 같다. 아마 set을 생성자로 가지는 ArrayList를 생성해서 참조값이 바뀐것 같다.
+						
 						Collections.sort(UserGetLottoNumbers);
 						System.out.println("내가 선택한 로또 숫자는 = " + UserGetLottoNumbers);
 					} 
@@ -94,7 +103,7 @@ public class lottoChoice extends JFrame {
 					result = "반자동";
 				}
 				while (UserGetLottoNumbers.size() < 6) {
-					int random = (int) (Math.random()*44 + 1);
+					int random = (int) (Math.random()*45 + 1);
 					UserGetLottoNumbers.add(random);
 					Collections.sort(UserGetLottoNumbers);
 				}
@@ -122,6 +131,26 @@ public class lottoChoice extends JFrame {
 			}
 		});	
 		/////////////////////////////////////////// 자동 버튼 만들기 마지막///////////////////////////////////////////////////////
+
+		
+		//////////////////////////////////////////////////확인 버튼 시작/////////////////////////////////////////////////////
+		checkButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (UserGetLottoNumbers.size() == 6) {
+					for (int i = 0; i < 6; i++) {
+						UserNumberImage[i].setIcon((ImageIcon)numberToImage.numberImage.get(UserGetLottoNumbers.get(i)));		// URL에서 숫자 0 이 이미지 1이다. 즉 숫자와 이미지가 완전히 대응되는게 아닌 이미지가 숫자 + 1의 값이 되었다.
+					}
+				}  
+				else {
+					System.out.println("번호 입력하세요");
+				}
+			}
+		});
+		
+		//////////////////////////////////////////////////확인 버튼 끝/////////////////////////////////////////////////////
+		
+		
 		
 		/////////////////////////////////////////// Reset버튼 만들기 시작 ////////////////////////////////////////////////////
 		resetButton.addActionListener(new ActionListener() {		// *오류가 났다 RESET을 하면 자동으로 받은 번호는 지워지지만 유저가 선택한 번호는 지워지지 않는다. **이거 오늘 꼭 해결한다
@@ -129,31 +158,25 @@ public class lottoChoice extends JFrame {
 																	// 2번 69번째 줄에서 new를 만들었기 때문에 참조가 바뀌었나?
 																	// 확실한건 Random으로 받아 넣은 List참조값과 클릭해서 즉 Select으로 받아 넣은 List참조값
 																	// 3번 논리 자체가 잘못되어서 갈아야 하나...? 제발.... 아니길
+																	// *****해결***** SET값도 없애야지만 초기화가 됐다.
+																	// RESET버튼을 눌리면 List값은 다 지워지지만 Set값은 남아있다. 그상태에서 숫자를 선택하면 지워지지않은 Set의 숫자와 클릭해서 생긴 Set숫자가 다 List안에 추가되게 된다.
 			@Override
 			public void actionPerformed(ActionEvent e) {
 //				choiceLottoNumbers.remove(choiceLottoNumbers);
 //				choiceLottoNumbers.remove(choiceLottoNumbers);
+				choiceLottoNumbers.clear();				// Set을 지우니깐 해결되었다!!!!					
 				UserGetLottoNumbers.clear();
 		//		UserGetLottoNumbers.removeAll(choiceLottoNumbers);		// 자동으로 받은 번호만 지워졌다.
 				System.out.println("리셋되면" + UserGetLottoNumbers.size());
+				for (int i = 0; i < 6; i++) {
+					UserNumberImage[i].setIcon(null);	// URL에서 숫자 0 이 이미지 1이다. 즉 숫자와 이미지가 완전히 대응되는게 아닌 이미지가 숫자 + 1의 값이 되었다.
+				}
 			}
 		});
 		/////////////////////////////////////////// Reset버튼 만들기 마지막 ////////////////////////////////////////////////////
-		
-		//////////////////////////////////////////////////확인 버튼 시작/////////////////////////////////////////////////////
-//		checkButton.addActionListener(new ActionListener() {
-//			@Override
-//			public void actionPerformed(ActionEvent e) {
-//				ChoiceNumberAll.setIcon((Icon) numberToImage.numberImage.get(SequenceChoiceLottoNumbers.get(0)));
-//			}
-//		});
-		
-		//////////////////////////////////////////////////확인 버튼 끝/////////////////////////////////////////////////////
-		
 
 
 		pnl.add(checkButton);
-		pnl.add(ChoiceNumberAll);
 		pnl.add(autoButton);
 		pnl.add(resetButton);
 		pnl.add(titleLabel);
