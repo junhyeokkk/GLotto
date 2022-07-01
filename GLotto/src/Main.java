e64c95757b418b08edd6981db9c4aeb3a5ec459f
Administrator <Administrator@SC-202106161522>
2022-07-01 17:33:44 +0900

    3일차

--- GLotto/src/Main.java
+++ GLotto/src/Main.java
@@ -1,6 +1,9 @@
-import java.awt.EventQueue;
+import java.awt.EventQueue; 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -9,120 +12,157 @@ import javax.swing.ImageIcon;
 import java.awt.Color;
 import javax.swing.JButton;
 
-public class Main {
+public class Main extends JFrame {
+	 Login log = new Login();
+   Login l2;
+   List<Member> memberList3 = new ArrayList<>(Arrays.asList());
+   public Main() {
+      
+      
+      
+      
+         getContentPane().setBackground(Color.WHITE);
+         setBounds(100, 100, 451, 610);
+         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         getContentPane().setLayout(null);
+         
+         JPanel panel = new JPanel();
+         panel.setBackground(Color.WHITE);
+         panel.setBounds(12, 10, 411, 133);
+         getContentPane().add(panel);
+         panel.setLayout(null);
+         
+         JLabel lblNewLabel = new JLabel("New label");
+         lblNewLabel.setBounds(34, 0, 349, 132);
+         lblNewLabel.setIcon(new ImageIcon("D:\\GLotto\\GLotto\\Image\\초기화면 이미지.jpg"));
+         panel.add(lblNewLabel);
+         
+         JPanel panel_1 = new JPanel();
+         panel_1.setBackground(Color.WHITE);
+         panel_1.setBounds(44, 153, 351, 418);
+         getContentPane().add(panel_1);
+         panel_1.setLayout(null);
+         
+         JButton btnNewButton = new JButton("구매");
+         btnNewButton.setBackground(Color.WHITE);
+         btnNewButton.setBounds(94, 10, 168, 57);
+         panel_1.add(btnNewButton);
+         
+         JButton btnMypage = new JButton("MyPage");
+         btnMypage.setBackground(Color.WHITE);
+         btnMypage.setBounds(94, 109, 168, 57);
+         panel_1.add(btnMypage);
+         
+         
+        
+         
+         for (int i = 0; i < log.memberList2.size(); i++) {
+            
+            memberList3.add(new Member(
+                    log.memberList2.get(i).getName(), 
+                    log.memberList2.get(i).getID(), 
+                    log.memberList2.get(i).getPw(),
+                    log.memberList2.get(i).getFnum(),
+                    log.memberList2.get(i).getBnum(),
+                    log.memberList2.get(i).getFPhone(),
+                    log.memberList2.get(i).getMPhone(),
+                    log.memberList2.get(i).getLPhone()
+                    ));
+         }
+         
+         
+         
+         System.out.println(log.wantIndexNumber);
+         System.out.println(log.saveId);
+         
+         btnMypage.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+              MyPage m = new MyPage();
+             m.getFrame().setVisible(true);
+               dispose();
+            }
+            
+         });
+         
+         JButton btnLogout = new JButton("Logout");
+         btnLogout.setBackground(Color.WHITE);
+         btnLogout.setBounds(94, 211, 168, 57);
+         panel_1.add(btnLogout);
+         
+         btnLogout.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+         
+              for (int i = 0; i < log.memberList2.size(); i++) {
+                  
+                  memberList3.add(new Member(
+                          log.memberList2.get(i).getName(), 
+                          log.memberList2.get(i).getID(), 
+                          log.memberList2.get(i).getPw(),
+                          log.memberList2.get(i).getFnum(),
+                          log.memberList2.get(i).getBnum(),
+                          log.memberList2.get(i).getFPhone(),
+                          log.memberList2.get(i).getMPhone(),
+                          log.memberList2.get(i).getLPhone()
+                          ));
+               }
+               
+               log.setVisible(true);
+               dispose();
+            }
+            
+         });
+         
+         JButton Exitbtn = new JButton("종료");
+         Exitbtn.setBackground(Color.WHITE);
+         Exitbtn.setBounds(94, 315, 168, 57);
+         panel_1.add(Exitbtn);
+         
+         JLabel qqq = new JLabel("New label");
+         qqq.setBounds(12, 49, 57, 15);   
+         qqq.setText(l2.m2.getName());
+         panel_1.add(qqq);
+         
+         Exitbtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               for (int i = 0; i < log.memberList2.size(); i++) {
+                  
+                  memberList3.add(new Member(
+                          log.memberList2.get(i).getName(), 
+                          log.memberList2.get(i).getID(), 
+                          log.memberList2.get(i).getPw(),
+                          log.memberList2.get(i).getFnum(),
+                          log.memberList2.get(i).getBnum(),
+                          log.memberList2.get(i).getFPhone(),
+                          log.memberList2.get(i).getMPhone(),
+                          log.memberList2.get(i).getLPhone()
+                          ));
+               }
+               
+               log.setVisible(true);
+               
+                
+            }
+         });
+   }
 
-	private JFrame frame;
-	
+   public static void main(String[] args) {
+      EventQueue.invokeLater(new Runnable() {
+         public void run() {
+            try {
+               Main window = new Main();
+               window.setVisible(true);
+            } catch (Exception e) {
+               e.printStackTrace();
+            }
+         }
+      });
+   }
 
-	/**
-	 * @return the frame
-	 */
-	public JFrame getFrame() {
-		return frame;
-	}
+   /**
+    * Create the application.
+    */
 
-	/**
-	 * @param frame the frame to set
-	 */
-	public void setFrame(JFrame frame) {
-		this.frame = frame;
-	}
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Main window = new Main();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the application.
-	 */
-	public Main() {
-		initialize();
-	}
-
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frame = new JFrame();
-		frame.getContentPane().setBackground(Color.WHITE);
-		frame.setBounds(100, 100, 451, 610);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
-		
-		JPanel panel = new JPanel();
-		panel.setBackground(Color.WHITE);
-		panel.setBounds(12, 10, 411, 133);
-		frame.getContentPane().add(panel);
-		panel.setLayout(null);
-		
-		JLabel lblNewLabel = new JLabel("New label");
-		lblNewLabel.setBounds(34, 0, 349, 132);
-		lblNewLabel.setIcon(new ImageIcon("D:\\GLotto\\GLotto\\Image\\초기화면 이미지.jpg"));
-		panel.add(lblNewLabel);
-		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBackground(Color.WHITE);
-		panel_1.setBounds(44, 153, 351, 418);
-		frame.getContentPane().add(panel_1);
-		panel_1.setLayout(null);
-		
-		JButton btnNewButton = new JButton("구매");
-		btnNewButton.setBackground(Color.WHITE);
-		btnNewButton.setBounds(94, 10, 168, 57);
-		panel_1.add(btnNewButton);
-		
-		JButton btnMypage = new JButton("MyPage");
-		btnMypage.setBackground(Color.WHITE);
-		btnMypage.setBounds(94, 109, 168, 57);
-		panel_1.add(btnMypage);
-		
-		btnMypage.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				MyPage m = new MyPage();
-				m.getFrame().setVisible(true);
-				frame.dispose();
-			}
-			
-		});
-		
-		JButton btnLogout = new JButton("Logout");
-		btnLogout.setBackground(Color.WHITE);
-		btnLogout.setBounds(94, 211, 168, 57);
-		panel_1.add(btnLogout);
-		
-		btnLogout.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				Login l = new Login();
-				l.getFrame().setVisible(true);
-				frame.dispose();
-			}
-			
-		});
-		
-		JButton Exitbtn = new JButton("종료");
-		Exitbtn.setBackground(Color.WHITE);
-		Exitbtn.setBounds(94, 315, 168, 57);
-		panel_1.add(Exitbtn);
-		
-		Exitbtn.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				 frame.setVisible(false);
-			}
-		});
-	}
 }
